d85a36994b23feab4dd63284a1e93ec5b07be385
tuankiet204n <150346287+tuankiet204n@users.noreply.github.com>
2023-11-23 14:57:11 +0700

    [BanAn] CRUD

---
+++ BUS_QLNhaHang/BUS_BanAn.cs
@@ -0,0 +1,38 @@
+﻿using DTO_QLNhaHang;
+using DAL_QLNhaHang;
+using System.Collections.Generic;
+using System.Data;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS_QLNhaHang
+{
+    public class BUS_BanAn
+    {
+        DAL_BanAn dalbanan = new DAL_BanAn();
+        public DataTable DanhSachBanAn()
+        {
+            return dalbanan.DanhSachBanAn();
+        }
+        public bool ThemBanAn(DTO_BanAn ba)
+        {
+            return dalbanan.ThemBanAn(ba);
+        }
+        public bool CapNhatBanAn(DTO_BanAn ba, string mabanan)
+        {
+            return dalbanan.CapNhatBanAn(ba, mabanan);
+        }
+
+        public object getDanhSachBanAn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool XoaBanAn(string mabanan)
+        {
+            return dalbanan.XoaBanAn(mabanan);
+        }
+    }
+}
