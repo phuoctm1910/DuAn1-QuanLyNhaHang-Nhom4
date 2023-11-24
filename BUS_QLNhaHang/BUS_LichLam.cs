2444c6e0d7c97f467b466d90faf0271e3070af3c
tuankiet204n <150346287+tuankiet204n@users.noreply.github.com>
2023-11-24 21:32:49 +0700

    [LichLam] CRU

---
+++ BUS_QLNhaHang/BUS_LichLam.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_QLNhaHang;
+using DTO_QLNhaHang;
+
+namespace BUS_QLNhaHang
+{
+    public class BUS_LichLam
+    {
+        DAL_LichLam dallichlam = new DAL_LichLam();
+        public DataTable DanhSachLichLam()
+        {
+            return dallichlam.DanhSachLichLam();
+        }
+        public bool ThemLichLam(DTO_LichLam ll)
+        {
+            return dallichlam.ThemLichLam(ll);
+        }
+        public bool CapNhatLichLam(DTO_LichLam ll, int id)
+        {
+            return dallichlam.CapNhatLichLam(ll, id);
+        }
+    }
+}
---
+++ DAL_QLNhaHang/DAL_LichLam.cs
@@ -0,0 +1,65 @@
+﻿using DTO_QLNhaHang;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL_QLNhaHang
+{
+    public class DAL_LichLam : DBConnect
+    {
+        public DataTable DanhSachLichLam()
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[DanhSachLichLam]";
+                cmd.Connection = _conn;
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                return dt;
+            }
+
+            finally { _conn.Close(); }
+        }
+        public bool ThemLichLam(DTO_LichLam ll)
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _conn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[InsertLichLam]";
+                cmd.Parameters.AddWithValue("LichLam", ll.LichLam);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+
+            }
+            finally { _conn.Close(); }
+            return false;
+        }
+        public bool CapNhatLichLam(DTO_LichLam ll, int id)
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _conn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[UpdateLichLam]";
+                cmd.Parameters.AddWithValue("id", id);
+                cmd.Parameters.AddWithValue("lichlam", ll.LichLam);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            finally { _conn.Close(); }
+            return false;
+        }
+    }
+}
