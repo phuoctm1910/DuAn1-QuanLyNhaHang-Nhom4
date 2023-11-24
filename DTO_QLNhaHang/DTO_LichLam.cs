1abcd6fdb19f60b33067bbc8ed223d028c87a9dd
tuankiet204n <150346287+tuankiet204n@users.noreply.github.com>
2023-11-24 21:32:49 +0700

    [LichLam] CRU

---
+++ DTO_QLNhaHang/DTO_LichLam.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QLNhaHang
+{
+    public class DTO_LichLam
+    {
+        private string lichLam;
+
+        public string LichLam
+        {
+            get { return lichLam; }
+            set { lichLam = value; }
+        }
+
+        public DTO_LichLam(string lichLam)
+        {
+            this.lichLam = lichLam;
+        }
+
+        public DTO_LichLam()
+        {
+        }
+    }
+}
