4b017c34b4e4e92cfc02f84cbd8a73abf5a3d0eb
anhdepzai3000 <150640877+anhdepzai3000@users.noreply.github.com>
2023-11-29 23:03:34 +0700

    [NhomMonAn]DTO, DAL, BUS Nhóm Món Ăn

---
+++ DAL_QLNhaHang/DAL_NhomMonAn.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO_QLNhaHang;
+
+namespace DAL_QLNhaHang
+{
+    public class DAL_NhomMonAn : DBconnect
+    {
+        public DataTable DanhSachNhomMonAn()
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[DanhSachNhomMonAn]";
+                cmd.Connection = _conn;
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                return dt;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally { _conn.Close(); }
+        }
+        public bool ThemNhomMonAn(DTO_NhomMonAn nma)
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _conn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[InsertNhomMonAn]";
+                cmd.Parameters.AddWithValue("tennhom", nma.TenNhomMonAn);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+
+            }
+            finally { _conn.Close(); }
+            return false;
+        }
+        public bool CapNhatNhomMonAn(DTO_NhomMonAn nma, string ma)
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _conn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[UpdateNhomMonAn]";
+                cmd.Parameters.AddWithValue("manhomMonan", ma);
+                cmd.Parameters.AddWithValue("tennhom", nma.TenNhomMonAn);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+
+            }
+            finally { _conn.Close(); }
+            return false;
+        }
+
+        public bool XoaNhomMonAn(string ma)
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _conn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[DeleteNhomMonAn]";
+                cmd.Parameters.AddWithValue("manhomMonan", ma);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+
+            }
+            finally { _conn.Close(); }
+            return false;
+        }
+    }
+}
+
---
+++ DTO_QLNhaHang/DTO_NhomMonAn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QLNhaHang
+{
+    public class DTO_NhomMonAn
+    {
+        private string tenNhomMonAn;
+
+        public string TenNhomMonAn { get { return tenNhomMonAn; } set { tenNhomMonAn = value; } }
+
+        public DTO_NhomMonAn()
+        {
+        }
+
+        public DTO_NhomMonAn(string tenNhomMonAn)
+        {
+            this.tenNhomMonAn = tenNhomMonAn;
+        }
+    }
+}
