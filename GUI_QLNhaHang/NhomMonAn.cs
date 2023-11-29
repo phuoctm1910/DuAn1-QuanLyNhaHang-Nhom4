fe060b19ca4c5ca32ae9f07b5f87831dea0261af
anhdepzai3000 <150640877+anhdepzai3000@users.noreply.github.com>
2023-11-29 23:24:52 +0700

    [NhomMonAn]Làm Mới

--- GUI_QLNhaHang/NhomMonAn.cs
+++ GUI_QLNhaHang/NhomMonAn.cs
@@ -111,5 +111,10 @@ namespace GUI_QLNhaHang
                 ResetValues();
             }
         }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+        }
     }
 }
