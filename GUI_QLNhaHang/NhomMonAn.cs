3f39b6726ca6682f10567cf8805db1413112f160
anhdepzai3000 <150640877+anhdepzai3000@users.noreply.github.com>
2023-11-29 23:10:19 +0700

    [NhomMonAn]Load Danh Sách Nhóm Món Ăn

--- GUI_QLNhaHang/NhomMonAn.cs
+++ GUI_QLNhaHang/NhomMonAn.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS_QLNhaHang;
+using DTO_QLNhaHang;
 
 namespace GUI_QLNhaHang
 {
     public partial class NhomMonAn : Form
     {
+        BUS_NhomMonAn busNMA = new BUS_NhomMonAn();
+        DTO_NhomMonAn nma = new DTO_NhomMonAn();
         public NhomMonAn()
         {
+            
             InitializeComponent();
         }
+        void LoadData()
+        {
+            txtMaNhomMonAn.Enabled = false;
+            dvDanhSachNhomMonAn.DataSource = busNMA.DanhSachNhomMonAn();
+            dvDanhSachNhomMonAn.Columns[0].HeaderText = "Mã Nhóm";
+            dvDanhSachNhomMonAn.Columns[1].HeaderText = "Tên Nhóm";
+        }
+        void ResetValues()
+        {
+            txtMaNhomMonAn.Clear();
+            txtTenNhomMonAn.Clear();
+        }
+
+        private void NhomMonAn_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            ResetValues();
+        }
+
     }
 }
