63b9b0eaa032b361a0978597dbca660413cf03b2
phuoctm1910 <nothinginthis465@gmail.com>
2023-11-30 16:05:21 +0700

    [GUI_QLNhaHang] Thêm form InHoaDon

---
+++ GUI_QLNhaHang/InHoaDon.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace GUI_QLNhaHang
+{
+    partial class InHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewerInHoaDon = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewerInHoaDon
+            // 
+            this.reportViewerInHoaDon.AutoSize = true;
+            this.reportViewerInHoaDon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewerInHoaDon.Location = new System.Drawing.Point(0, 0);
+            this.reportViewerInHoaDon.Name = "reportViewerInHoaDon";
+            this.reportViewerInHoaDon.ServerReport.BearerToken = null;
+            this.reportViewerInHoaDon.Size = new System.Drawing.Size(867, 548);
+            this.reportViewerInHoaDon.TabIndex = 0;
+            // 
+            // InHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(867, 548);
+            this.Controls.Add(this.reportViewerInHoaDon);
+            this.Name = "InHoaDon";
+            this.Text = "InHoaDon";
+            this.Load += new System.EventHandler(this.InHoaDon_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewerInHoaDon;
+    }
+}
